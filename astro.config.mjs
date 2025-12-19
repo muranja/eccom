08c7300bea81e33d5981ce2dcae951db43bb8e92
Simon Muranja <simonmuranja365@gmail.com>
2025-12-20 00:37:34 +0300

    mdx overhaul

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,14 @@ import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 
 import mdx from '@astrojs/mdx';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://sasagadgets.com',
+    // output: 'static', // Default in Astro 5
+    adapter: node({
+        mode: 'standalone',
+    }),
     integrations: [tailwind(), react(), sitemap(), mdx()],
 });
